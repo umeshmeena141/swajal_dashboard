294636159219e599f3b08788e108bffc9fbd6916
Umesh Meena <31372681+umeshmeena141@users.noreply.github.com>
2018-08-03 12:47:54 +0530

    Update db_connection.php

--- src/assets/Php/db_connection.php
+++ src/assets/Php/db_connection.php
@@ -3,21 +3,11 @@
     error_reporting(0);
 
 	define("DB_SERVER", "localhost");
-	define("DB_USER", "saurya_iotprod");
-	define("DB_PASS", "cJL5vS4.nzX[");
-	define("DB_NAME", "saurya_rmsprod");
+	define("DB_USER", "user");
+	define("DB_PASS", "pass");
+	define("DB_NAME", "dbname");
 	
-	// define("DB_SERVER", "localhost");
-	// define("DB_USER", "root");
-	// define("DB_PASS", "ume12345");
-	// define("DB_NAME", "swajal");
 
-
-	
-	// define("DB_SERVER", "localhost");
-	// define("DB_USER", "root");
-	// define("DB_PASS", "yashX8mysql");
-	// define("DB_NAME", "swajal");
 
 	// 1. Create a database connection
     $conn = new mysqli(DB_SERVER, DB_USER, DB_PASS, DB_NAME);
